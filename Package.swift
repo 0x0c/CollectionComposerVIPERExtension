e84f183fe90e2d69f1c390bdd30f77389da5ce6f
Akira <akira.matsuda@me.com>
2023-11-08 16:50:11 +0900

    Fix error

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "CollectionComposerVIPERExtension",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14),
+        .macOS(.v10_15)
     ],
     products: [
         .library(
