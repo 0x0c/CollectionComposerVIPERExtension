12417501237fb6d1f16feb44d35f695ebac1ec22
Akira <akira.matsuda@me.com>
2024-01-18 13:22:40 +0900

    Fix protocol name

--- Sources/ComposedViewInteractorOutput.swift
+++ Sources/ComposedViewInteractorOutput.swift
@@ -1,5 +1,5 @@
 //
-//  ComposedViewPresenterOutput.swift
+//  ComposedViewInteractorOutput.swift
 //  
 //
 //  Created by Akira Matsuda on 2024/01/18.
@@ -8,6 +8,6 @@
 import CollectionComposer
 
 @MainActor
-public protocol ComposedViewPresenterOutput: AnyObject {
+public protocol ComposedViewInteractorOutput: AnyObject {
     func needsUpdateSections()
 }
