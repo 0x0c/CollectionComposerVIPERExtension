133cfc926aacf5465225169dd67b7fe377a4fc27
Akira <akira.matsuda@me.com>
2024-01-22 15:39:09 +0900

    Remove ComposedViewFetchablePresenterInput

--- Sources/ComposedViewFetchableInteractorInput.swift
+++ Sources/ComposedViewFetchableInteractorInput.swift
@@ -11,12 +11,4 @@ public protocol ComposedViewFetchableInteractorInput: AnyObject {
     associatedtype Entity
 
     var storage: Entity? { get }
-
-    func fetch(force: Bool) async throws
-}
-
-public extension ComposedViewFetchableInteractorInput {
-    func fetch(force: Bool = false) async throws {
-        try await fetch(force: force)
-    }
 }
