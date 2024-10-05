54f3c8b8bd453cdae397d4220377b3781203cbf9
Akira MATSUDA <akira.matsuda@me.com>
2024-10-06 01:08:26 +0900

    Update minimum requirements.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "CollectionComposerVIPERExtension",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v15),
         .macOS(.v10_15)
     ],
     products: [
